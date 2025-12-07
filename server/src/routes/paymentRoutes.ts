6b14aa7da9426e1701d47a3e6d1252fa613158cd
Makfront0123 <ing.sistemas2019armando@gmail.com>
2025-12-06 23:54:54 -0500

    refactor routes

--- server/src/routes/paymentRoutes.ts
+++ server/src/routes/paymentRoutes.ts
@@ -1,20 +1,19 @@
-import express from 'express';
-import { PaymentController } from '../controllers/paymentController';
-import { adminMiddleware } from '../middleware/adminMiddleware';
-import { authMiddleware } from '../middleware/authMiddleware';
-import { blockAdminMiddleware } from '../middleware/blockAdminMiddleware';
+import { Router } from "express";
+import { PaymentController } from "../controllers/paymentController";
+import { PaymentService } from "../services/paymentService";
+import { AppDataSource } from "../data-source";
 
+const router = Router();
 
-const router = express.Router();
-const paymentController = new PaymentController();
-
-router.post('/', authMiddleware, blockAdminMiddleware, paymentController.createPayment.bind(paymentController));
-router.get('/:id', authMiddleware, paymentController.getPaymentById.bind(paymentController));
-router.get('/', authMiddleware, paymentController.getAllPayments.bind(paymentController));
-router.delete('/:id', authMiddleware, adminMiddleware, paymentController.deletePayment.bind(paymentController));
-router.put('/:id', authMiddleware, adminMiddleware, paymentController.updatePayment.bind(paymentController));
-
-router.put('/:id/complete', authMiddleware, adminMiddleware, paymentController.completePayment.bind(paymentController));
-router.put('/:id/cancel', authMiddleware, adminMiddleware, paymentController.cancelPayment.bind(paymentController));
+const paymentService = new PaymentService(AppDataSource);
+const paymentController = new PaymentController(paymentService);
+ 
+router.get("/", paymentController.getAllPayments.bind(paymentController));
+router.post("/", paymentController.createPayment.bind(paymentController));
+router.get("/:id", paymentController.getPaymentById.bind(paymentController));
+router.delete("/:id", paymentController.deletePayment.bind(paymentController));
+router.put("/:id", paymentController.updatePayment.bind(paymentController));
+router.post("/:id/complete", paymentController.completePayment.bind(paymentController));
+router.post("/:id/cancel", paymentController.cancelPayment.bind(paymentController));
 
 export default router;
