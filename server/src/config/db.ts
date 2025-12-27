53df06248b629269c2940faca04ef27d3f2d7a6e
Makfront0123 <ing.sistemas2019armando@gmail.com>
2025-12-27 01:26:32 -0500

    database

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,11 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,       // raffle-web-db-raffle-web.h.aivencloud.com
+  user: process.env.DB_USER,       // tu usuario
+  password: process.env.DB_PASSWORD, // tu password
+  database: process.env.DB_DATABASE, // defaultdb
+  port: 12104,
+  ssl: { rejectUnauthorized: true } // porque dice ssl-mode=REQUIRED
 });
 
- 
 console.log('Database pool created');
