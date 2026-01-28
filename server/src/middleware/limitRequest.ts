d64b438a1bc32a2b26f09bc74b66495ef5615d38
Makfront0123 <ing.sistemas2019armando@gmail.com>
2026-01-27 21:59:16 -0500

    change auth limiter to 30 request

--- server/src/middleware/limitRequest.ts
+++ server/src/middleware/limitRequest.ts
@@ -10,7 +10,7 @@ export const globalLimiter = rateLimit({
 
 export const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 10,
+    max: 30,
     message: {
         error: "Demasiados intentos de autenticación",
     },
